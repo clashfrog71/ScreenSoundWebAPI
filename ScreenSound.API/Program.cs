1735b4b50b63f8ed0bbaa4a933caf773cb47c3f2
Tiago Pereira <clashfrog71@gmail.com>
2025-06-29 23:54:48 -0300

    Listar artistas no projeto web e mudar a visibilidade do DAL e do screensoundcontext

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,6 +1,16 @@
+using ScreenSound.Modelos;
+using System.Text.Json.Serialization;
+using ScreenSound.Banco;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+app.MapGet("/", () => {
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    dal.Listar();
+
+});
 
 app.Run();
