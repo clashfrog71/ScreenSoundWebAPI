c45d133026b5a84fbcebe6873b2ffd26437206ea
Tiago Pereira <clashfrog71@gmail.com>
2025-07-01 23:47:32 -0300

    Adicionar metodo na api para achar artistas pelo nome

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -7,10 +7,19 @@ var app = builder.Build();
 
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-app.MapGet("/", () => {
+app.MapGet("/", () =>
+{
     var dal = new DAL<Artista>(new ScreenSoundContext());
-    return dal.Listar();
-
+    return Results.Ok(dal.Listar());
+});
+app.MapGet("/artistas/{nome}", (string nome) => {
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    var artista = dal.RecuperarPor(a => a.Nome.Equals(nome, StringComparison.OrdinalIgnoreCase));
+if (artista != null)
+    {
+        Results.NotFound();
+    }
+return Results.Ok(artista);
 });
 
 app.Run();
