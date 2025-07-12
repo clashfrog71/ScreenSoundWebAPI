f428a1fe1783c2bf127c4c2d3a3b3773a463875f
Tiago Pereira <clashfrog71@gmail.com>
2025-07-11 23:59:45 -0300

    Adicionar o swagger no endpoint

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -8,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 ArtistaEndpoint.Artista(app);
 MusicaEndpoint.Musica(app);
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
