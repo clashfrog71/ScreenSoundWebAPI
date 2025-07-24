17fff1a40544eeb2e273187b39647b030a909ed9
Tiago Pereira <clashfrog71@gmail.com>
2025-07-23 22:35:29 -0300

    Adicionando genero response

---
+++ ScreenSound.API/Response/GeneroResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ScreenSound.API.Response;
+
+public class GeneroResponse
+{
+    public int Id { get; set; }
+    public string? genero { get; set; } = string.Empty;
+    public string? descricao { get; set; } = string.Empty;
+    public ICollection<ArtistaResponse> artistaResponses { get; set; } = new List<ArtistaResponse>();
+}
