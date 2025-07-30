bf9d025d86271edc724399addb1873c963b85fd2
Tiago Pereira <clashfrog71@gmail.com>
2025-07-29 23:44:56 -0300

    Adicionando o Tostring em genero

--- ScreenSound.Shered.Modelos/Modelos/Genero.cs
+++ ScreenSound.Shered.Modelos/Modelos/Genero.cs
@@ -8,4 +8,10 @@ public class Genero
     public string? genero { get; set; } = string.Empty;
     public string? descricao { get; set; } = string.Empty;
     public ICollection<Artista>? artistas { get; set; } = new List<Artista>();
+    public override string ToString()
+    {
+        return @$"Id: {Id}
+        Genero: {genero}
+        Descricao: {descricao}";
+    }
 }
